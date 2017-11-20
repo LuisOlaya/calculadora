85b9e152fc4bee3d6ec781f4ba4a57cf4cd78461
LuisOlaya <LuisOlaya@DESKTOP-GJQI7P6>
2017-11-20 11:15:39 -0500

    iniciacion de metodos

---
+++ Calculadora/src/calculadora/calcu.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author LuisOlaya
+ */
+public class calcu {
+    private int ans;
+
+    public calcu() {
+        ans = 0;
+    }
+    
+    public int suma(int a, int b){
+        ans = a + b;
+        return ans;
+    }
+    public int resta(int a, int b){
+        ans = a - b;
+        return ans;
+    }
+    public int multi(int a, int b){
+        ans = a * b;
+        return ans;
+    }
+    public double division(int a, int b){
+        ans = (int)(a / b);
+        return ans;
+    }
+    public void limpiar(){
+        ans = 0;
+    }
+    public int ans(){
+        return ans;
+    }
+}
